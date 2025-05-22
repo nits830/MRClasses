7786a602d8c4af2ede6e6e739c912e7acc23cbad
Nitish Gupta <nitishgupta830@gmail.com>
2025-05-22 20:59:20 +0530

    Signup error fixed

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ const fileRoutes = require('./routes/fileRoutes');
 const path = require('path');
 
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
 // Connect to MongoDB
 connectDB();
