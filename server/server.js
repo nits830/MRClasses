bc8b8b041db37a816c4f0f50911bccf624332cc0
Nitish Gupta <nitishgupta830@gmail.com>
2025-05-22 21:28:50 +0530

    Server port fixed

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ const fileRoutes = require('./routes/fileRoutes');
 const path = require('path');
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT ;
 
 // Connect to MongoDB
 connectDB();
